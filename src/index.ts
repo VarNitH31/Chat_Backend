692a1cee522e0793bde23a315808360b4ae0f12f
VarNitH31 <varnithdramesh@gmail.com>
2025-02-26 13:51:10 +0530

    basic workings

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { WebSocketServer , WebSocket} from "ws";
+
+const wss=new WebSocketServer({port:8080});
+
+interface User{
+    socket:WebSocket,
+    room:string;
+    userName:string
+}
+
+let userCount=0;
+let allSockets:User[]=[];
+
+wss.on("connection",(socket)=>{
+    userCount=userCount+1;
+    console.log(`user connected  #${userCount} `);
+    socket.on("message",(message)=>{
+        // console.log("message recieved "+message.toString()+" from user");
+        // // socket.send(`${message.toString()} sent from the server `)
+        // allSockets.forEach((s)=>{
+        //     s.send(message.toString()+":sent from the server")
+        // })
+        //@ts-ignore
+        const parsedMessage=JSON.parse(message);
+        if(parsedMessage.type==="join"){
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId,
+                userName:parsedMessage.payload.userName
+            })
+            console.log(`${parsedMessage.payload.roomId}`)
+            let currentUserRoom=parsedMessage.payload.roomId;
+            for (let i = 0; i < allSockets.length; i++) {
+                if(currentUserRoom==allSockets[i].room){
+                    // if(parsedMessage.payload.userName==allSockets[i].userName){
+                    //     socket.send(JSON.stringify({
+                    //         type:"error",
+                    //         message:"dupplicate username"
+                    //     }))
+
+                    //     socket.close();
+                    //     return;
+                    // }
+                    allSockets[i].socket.send(JSON.stringify({
+                        type:"joined",
+                        userName:parsedMessage.payload.userName
+                    }))
+                }
+            }
+
+        }
+        if(parsedMessage.type==="chat"){
+            // const currentUSerRoom=allSockets.find((x)=>x.socket == socket).room
+            let currentUserRoom=null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if(allSockets[i].socket==socket){
+                    currentUserRoom=allSockets[i].room;
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if(currentUserRoom==allSockets[i].room){
+                    // allSockets[i].socket.send(`${parsedMessage.payload}`)
+                    allSockets[i].socket.send(JSON.stringify({
+                        message:parsedMessage.payload.message,
+                        userName:parsedMessage.payload.userName
+                    }))
+                }
+                
+            }
+        }
+    })
+    // allSockets.push(socket);
+
+    socket.on("disconnect",(ds)=>{
+       allSockets= allSockets.filter((s)=>{s!==ds;})
+       userCount=userCount-1;
+    })
+
+})
